56741f35b226d1de3bd3dfc77db17ec59c15f7c4
Oloruntobi Madamori <108978988+64eyes@users.noreply.github.com>
2022-11-19 17:05:57 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import "./App.css";
+import Desktop from "./components/desktop_1/Desktop";
+
+function App() {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Desktop />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
